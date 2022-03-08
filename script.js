24d6c21bfdd69074eb096dc47061d4c086d4fea4
Stanislav Minko <minkostas@gmail.com>
2022-03-08 15:18:15 +0300

    fix: cart-items btn

--- script.js
+++ script.js
@@ -15,8 +15,7 @@ let cartItem = document.querySelector('.cart-items-container');
 document.querySelector('#cart-btn').onclick = () =>{
     cartItem.classList.toggle('active');
     navbar.classList.remove('active');
-    searchForm.classList.remove('active');
-    cartItem.classList.remove('active');
+
 }
 window.onscroll = () => {
     navbar.classList.remove('active');
